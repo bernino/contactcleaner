f7f7a00ab2a03d97468943066837cb18847e0a8f
Viktor Petersson <self@vpetersson.com>
2020-11-11 19:32:36 +0000

    Code cleanup and documentation.

--- pyhunting.py
+++ pyhunting.py
@@ -1,47 +1,66 @@
-from pyhunter import PyHunter
+import configparser
 import pandas as pd
-import json
+import sys
+import os
+from pyhunter import PyHunter
 from pandas.io.json import json_normalize
 import validators
-import os
-import configparser
 
-if os.getenv('PYHUNTER'):
-    pyhunterapikey = os.getenv('PYHUNTER')
-else:
+
+if os.getenv('HUNTER_API_KEY'):
+    HUNTER_API_KEY = os.getenv('HUNTER_API_KEY')
+elif os.path.isfile('secrets'):
     config = configparser.ConfigParser()
     config.read('secrets')
-    pyhunterapikey = config['pyhunter']['api']
+    HUNTER_API_KEY = config['pyhunter']['api']
+else:
+    HUNTER_API_KEY = False
 
-hunter = PyHunter(pyhunterapikey)
 
-source_file = 'domainresolution-all-banks-clean-no-dupes.csv'
-out_file = 'pyhunterout.csv'
-domains = 'domain'
-organisation = 'Firm'
+def main():
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-df = pd.read_csv(source_file)
-df = df[507:]
+    if not os.path.isfile(input_file):
+        print("Input file doesn't exist. Exiting.")
+        sys.exit(1)
 
-normalised2 = pd.DataFrame()
+    if os.path.isfile(output_file):
+        print("Output file ({}) exists already. Exiting.".format(output_file))
+        sys.exit(1)
 
-# just in case there are duplicate domains we don't want to API call twice
-df = df.drop_duplicates(subset=['domain'])
+    if not HUNTER_API_KEY:
+        print("Hunter API Key missing. Exiting.")
+        sys.exit(1)
 
-for index, row in df.iterrows():
-    domain = row[domains]
-    # validators.domain does exactly that - nifty little tool
-    # also we only want to lookup unique domains
-    if validators.domain(domain) and domain != 'wikipedia.org' and domain != '4icu.org':
-        print("processing {} row {}".format(domain, index))
-        try:
-            results = hunter.domain_search(domain, limit=100, emails_type='personal')
-        except:
-            results = None
+    hunter = PyHunter(HUNTER_API_KEY)
+    df = pd.read_csv(input_file)
+    organisation = 'Firm'
 
-        normalised = json_normalize(results['emails'])
-        normalised['org'] = row[organisation]
-        normalised.to_csv('list.csv', mode='a', header=False, encoding='utf-8')
-        normalised2 = normalised2.append(normalised, sort=True)
+    normalised2 = pd.DataFrame()
 
-normalised2.to_csv(out_file)
+    # Just in case there are duplicate domains we don't want to API call twice
+    df = df.drop_duplicates(subset=['Domain'])
+
+    for index, row in df.iterrows():
+        domain = row['Domain']
+        # validators.domain does exactly that - nifty little tool
+        # also we only want to lookup unique domains
+        if validators.domain(domain) and domain != 'wikipedia.org' and domain != '4icu.org':
+            print("Processing {} on row {}".format(domain, index))
+
+            # Had to remove limit=100 as it broke the client
+            results = hunter.domain_search(domain, emails_type='personal')
+            if not results:
+                print("No records found....")
+                break
+
+            normalised = json_normalize(results['emails'])
+            normalised['org'] = row[organisation]
+            normalised.to_csv(input_file, mode='a', header=False, encoding='utf-8')
+            normalised2 = normalised2.append(normalised, sort=True)
+
+    normalised2.to_csv(output_file)
+
+if __name__ == "__main__":
+    main()
